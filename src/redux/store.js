abbfa04ef6dbdf159ac1be77284aaed3e0d820d1
Gabriel Valverde <gabriel_gom@hotmail.com>
2021-02-01 15:38:48 -0300

    Add: Redux devtools extension support

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import products from './modules/products';
 
-const store = createStore(combineReducers({ products }), applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  combineReducers({ products }),
+  composeEnhancers(applyMiddleware(thunk)),
+);
 
 export default store;
