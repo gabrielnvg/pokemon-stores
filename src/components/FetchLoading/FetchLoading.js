8b11a4ef31e06e287a66bdeb593666780d5dfad3
Gabriel Valverde <gabriel_gom@hotmail.com>
2021-02-01 20:55:50 -0300

    Add: Material-UI CircularProgress on FetchLoading

--- src/components/FetchLoading/FetchLoading.js
+++ src/components/FetchLoading/FetchLoading.js
@@ -1,10 +1,30 @@
 import React, { memo } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 import styles from './FetchLoading.module.scss';
 
+const useStyles = makeStyles(() => ({
+  root: {
+    marginTop: '20px',
+  },
+  circle: {
+    color: process.env.STORE.colorDark,
+  },
+}));
+
 function FetchLoading() {
+  const classes = useStyles();
+
   return (
     <div className={styles['fetch-loading']}>
-      Capturing the Pokémon, please wait...
+      <p>Capturing the Pokémon, please wait...</p>
+
+      <CircularProgress
+        className={[classes.root, classes.circle]}
+        color="secondary"
+      />
     </div>
   );
 }
